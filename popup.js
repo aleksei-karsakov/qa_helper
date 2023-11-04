4be6c78f145592de3e72a911f4d13c706b4fad89
aleksei-karsakov <118712037+aleksei-karsakov@users.noreply.github.com>
2023-11-04 17:22:38 +0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,25 @@
+const startBtn = document.getElementById("startBtn");
+const YT_DATE = /.+\/\/track.ecwid.com\/youtrack\/issue\/(?<issue>[^\/]+)/;
+
+startBtn.addEventListener("click",() => {    
+    chrome.tabs.query({active: true}, (tabs) => {
+        const tab = tabs[0];
+        const url = tab.url;
+        let YT = YT_DATE.exec(url); 
+        //Тут мы получаем тип тикета
+        let groupTitle=url[(url.indexOf('issue'))+6];
+        //Тут мы получаем номер тикета
+        let i = url.indexOf('-')+1;
+        for (let i = (url.indexOf('-')+1); !isNaN(url[i]); i++) {
+            groupTitle += url[i];
+            }
+        // alert(groupTitle);   
+        if (YT.groups.issue) {
+            chrome.tabs.group({tabIds:tab.id},function (groupId){
+                chrome.tabGroups.update(groupId, {collapsed:false, title:groupTitle});
+            }); 
+        } else {
+            alert("There are no active tabs");
+        }
+    })
+        })   
