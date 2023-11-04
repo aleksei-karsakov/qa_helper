5c091d470addf917ff4ea617ed75084bd18dd326
Aleksei Karsakov <aleksei.karsakov@lightspeedhq.com>
2023-11-04 19:11:25 +0400

    test

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 const startBtn = document.getElementById("startBtn");
+const checkVerify = document.getElementById("verify_test");
+const checkUnit = document.getElementById("unit_test");
 const YT_DATE = /.+\/\/track.ecwid.com\/youtrack\/issue\/(?<issue>[^\/]+)/;
 
 startBtn.addEventListener("click",() => {    
     chrome.tabs.query({active: true}, (tabs) => {
         const tab = tabs[0];
         const url = tab.url;
+        //Проверяем, это тикет Youtrack или нет
         let YT = YT_DATE.exec(url); 
         //Тут мы получаем тип тикета
         let groupTitle=url[(url.indexOf('issue'))+6];
@@ -16,9 +19,16 @@ startBtn.addEventListener("click",() => {
         if (YT.groups.issue) {
             chrome.tabs.group({tabIds:tab.id},function (groupId){
                 chrome.tabGroups.update(groupId, {collapsed:false, title:groupTitle});
-            }); 
+            });
+        //Смотрим на чекбокс с верифаями и добавляем в созданную группу страницу с верифаями   
+        if ( checkVerify.checked ) {
+                alert("Добавляю страницу с Верифаями")}
+        //Смотрим на чекбокс с юнитами и добавляем в созданную группу страницу с юнитами
+        if (checkUnit) {
+            alert("Добавляю страницу с Юнитами");
+        };
         } else {
-            alert("There are no active tabs 666");
+            alert("There are no active tabs");
         }
     })
         })   
