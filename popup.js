2c8b37359f436684416319ae9e6c44e8b8462dca
Aleksei Karsakov <aleksei.karsakov@lightspeedhq.com>
2023-12-13 00:22:28 +0400

    123

--- popup.js
+++ popup.js
@@ -2,7 +2,9 @@ const startBtn = document.getElementById("startBtn");
 const checkVerify = document.getElementById("verify_test");
 const checkUnit = document.getElementById("unit_test");
 const YT_DATE = /.+\/\/track.ecwid.com\/youtrack\/issue\/(?<issue>[^\/]+)/;
-let elem;
+
+
+
 startBtn.addEventListener("click", () => {
     chrome.tabs.query({ active: true }, (tabs) => {
         const tab = tabs[0];
@@ -22,32 +24,37 @@ startBtn.addEventListener("click", () => {
                 {
                     target: { tabId: tab.id, allFrames: true },
                     func: () => {
-                        elem = document.querySelector('body > div.app > div > div > div.app__view > yt-analytics-zone > yt-feature-zone > yt-issue-view > yt-issue-layout > div > div.yt-issue-layout__sidebar-container > div > yt-issue-layout-sidebar > div.yt-flex-box.yt-flex-column.yt-issue-sidebar-container > div > div > yt-issue-fields-panel > div > table > tbody > tr:nth-child(12) > td.yt-issue-key-value-list__column.yt-issue-key-value-list__column_value > span.yt-issue-fields-panel__field-value.ring-link.ring-link_pseudo > span > yt-events-animator > yt-event-animate > span > span > yt-issue-custom-field-simple > button').innerText;
-                        chrome.runtime.sendMessage({ elem: elem });
+                        branch = document.querySelector('body > div.app > div > div > div.app__view > yt-analytics-zone > yt-feature-zone > yt-issue-view > yt-issue-layout > div > div.yt-issue-layout__sidebar-container > div > yt-issue-layout-sidebar > div.yt-flex-box.yt-flex-column.yt-issue-sidebar-container > div > div > yt-issue-fields-panel > div > table > tbody > tr:nth-child(12) > td.yt-issue-key-value-list__column.yt-issue-key-value-list__column_value > span.yt-issue-fields-panel__field-value.ring-link.ring-link_pseudo > span > yt-events-animator > yt-event-animate > span > span > yt-issue-custom-field-simple > button').innerText;
+                        title = document.querySelector('body > div.app > div > div > div.app__view > yt-analytics-zone > yt-feature-zone > yt-issue-view > yt-issue-layout > div > div.yt-issue-layout__wrapper > div > yt-issue-layout-body > yt-issue-body > yt-dropzone > div > ng-transclude > yt-analytics-zone > div > yt-events-animator > yt-event-animate:nth-child(1) > h1').innerText;
+                        chrome.runtime.sendMessage({ branch: branch, title: title });
                     }
                 });
-            //Получаем значение ветки тикета, создаем группу
+            //Получаем значение ветки и название тикета, создаем группу
             chrome.runtime.onMessage.addListener(function (message) {
-                let elem = message.elem;
-                const VerifyUrl = 'https://devcity.ecwid.com/buildConfiguration/Tests_UpsourceVerify?branch=' + elem + '&buildTypeTab=overview&mode=builds';
-                const UnitUrl = 'https://devcity.ecwid.com/buildConfiguration/Tests_UnitTestCoverage?branch=' + elem + '&buildTypeTab=overview&mode=builds';
+                let branch = message.branch;
+                let title = message.title;
+                const VerifyUrl = 'https://devcity.ecwid.com/buildConfiguration/Tests_UpsourceVerify?branch=' + branch + '&buildTypeTab=overview&mode=builds';
+                const UnitUrl = 'https://devcity.ecwid.com/buildConfiguration/Tests_UnitTestCoverage?branch=' + branch + '&buildTypeTab=overview&mode=builds';
+                groupTitle = groupTitle + ' ' + title;
                 chrome.tabs.group({ tabIds: tab.id }, function (groupId) {
                     chrome.tabGroups.update(groupId, { collapsed: false, title: groupTitle });
-                    chrome.tabs.create({ url: VerifyUrl, active: false }, function (verify) {
-                        chrome.tabs.group({ tabIds: verify.id, groupId });
-                    });
-                    chrome.tabs.create({ url: UnitUrl, active: false }, function (unit) {
-                        chrome.tabs.group({ tabIds: unit.id, groupId });
-                    });
+                    //Добавляем в группу вкладку с верифай тестами, если чекбокс нажат
+                    if (checkVerify.checked) {
+                        chrome.tabs.create({ url: VerifyUrl, active: false }, function (verify) {
+                            chrome.tabs.group({ tabIds: verify.id, groupId });
+                        });
+                    };
+                    //Добавляем в группу вкладку с юнит тестами, если чекбокс нажат
+                    if (checkUnit.checked) {
+                        chrome.tabs.create({ url: UnitUrl, active: false }, function (unit) {
+                            chrome.tabs.group({ tabIds: unit.id, groupId });
+                        });
+                    };
                 });
             });
         } else {
             alert("There are no active tabs");
         }
     });
-    //Смотрим на чекбокс с верифаями и добавляем в созданную группу страницу с верифаями   
-
-    //Смотрим на чекбокс с юнитами и добавляем в созданную группу страницу с юнитами
-
 });
 
